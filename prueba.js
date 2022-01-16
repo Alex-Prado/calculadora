c4eac1af7657540c6acd4e74cb45c618cbd7fee3
AlexPrado <alexprado2808@gmail.com>
2022-01-15 23:35:30 -0500

    solucionando el problema de los 0 y los decimales en prueba.js en la linea 43 hasta la 52

--- prueba.js
+++ prueba.js
@@ -1,12 +1,12 @@
 let a = {
-  teclas          : document.querySelectorAll("#calculadora ul button"),
-  operaciones     : document.querySelector("#operaciones"),
-  digito          : null,
-  accion          : null,
-  cantidadSigno   : false,
-  cantidadDecimal : false,
-  resultado       : false,
-  cero            : false,
+  teclas: document.querySelectorAll("#calculadora ul button"),
+  operaciones: document.querySelector("#operaciones"),
+  digito: null,
+  accion: null,
+  cantidadSigno: false,
+  cantidadDecimal: false,
+  resultado: false,
+  cero: false,
 };
 
 let b = {
@@ -21,37 +21,54 @@ let b = {
   },
 
   Mostrar: (accion, digito) => {
+    
+
     switch (accion) {
       case "signo":
         if (!a.cantidadSigno && a.operaciones.innerHTML != 0) {
           a.operaciones.innerHTML += digito;
-          a.cantidadSigno   = true;
+          a.cantidadSigno = true;
           a.cantidadDecimal = false;
-          a.resultado       = false;
+          a.resultado = false;
         }
         break;
 
       case "numero":
+        let cortado = a.operaciones.innerHTML.split(/[+/*-]/);
+        let numero = a.operaciones.innerHTML;
 
         if (a.operaciones.innerHTML == 0) {
           a.operaciones.innerHTML = digito;
         } else {
-            if (a.resultado) {
-                a.operaciones.innerHTML = digito;
-                a.resultado             = false;
-                a.cantidadSigno         = false
+          if (parseInt(cortado[1]) == 0) {
+           
+            if (numero.substring(numero.length, numero.length-1) == '.') {
+              a.operaciones.innerHTML += digito;
             }else{
-                a.operaciones.innerHTML += digito;
-                a.cantidadSigno = false;
+
+              const str = numero.replace(/.$/, digito);
+              a.operaciones.innerHTML = str;
+              console.log(numero.substring(numero.length, numero.length-1));
             }
+          } else {
+            if (a.resultado) {
+              a.operaciones.innerHTML = digito;
+              a.resultado = false;
+              a.cantidadSigno = false;
+            } else {
+              a.operaciones.innerHTML += digito;
+              a.cantidadSigno = false;
+            }
+          }
         }
+
         break;
 
       case "decimal":
-        if (!a.cantidadDecimal && a.operaciones.innerHTML != 0) {
+        if (!a.cantidadDecimal && a.operaciones.innerHTML != 0 ) {
           a.operaciones.innerHTML += digito;
           a.cantidadDecimal = true;
-          a.resultado = false
+          a.resultado = false;
         }
         break;
 
@@ -61,7 +78,7 @@ let b = {
         break;
 
       case "limpiar":
-        console.log('limpiar')
+        console.log("limpiar");
         a.operaciones.innerHTML = 0;
         break;
     }
